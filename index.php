a8ead9557fdeb010a06c143d1276aa95d050ab98
LPWiseband <104561870+LPWiseband@users.noreply.github.com>
2022-04-29 12:29:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 Bonjour Lucas
-<input type="text" id="desktop_search_value" name="LPGUITARS" placeholder="Rechercher un produit" class="ui-autocomplete-input" autocomplete="off">
+<?php
+print "hello world ".time();
+?>
+<html>
+<body>
+hello world
+</body>
+</html> 
+
